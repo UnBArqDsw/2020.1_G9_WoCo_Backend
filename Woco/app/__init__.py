892935c4db257ac9df1b632e912b09ce3107ae0a
ZarathosDeath <nando_rhapsody@hotmail.com>
2020-10-22 17:35:56 -0300

    feat: add exercise model

--- Woco/app/__init__.py
+++ Woco/app/__init__.py
@@ -2,11 +2,24 @@ import os
 from flask import Flask
 import sqlite3
 import db.database
-from db.database import SqliteDB
+# from db.database import SqliteDB
+from db.database import db
 
-app = Flask(__name__)
+def createApp(dbUri: str) -> Flask:
+  app = Flask(__name__)
+  app.config['SQLALCHEMY_DB_URI'] = dbUri
+  # initialize db with the flask app
+  db.init_app(app)
 
-@app.route("/")
+  # register blueprints or routes
+  with app.app_context():
+    db.create_all()
+
+  return app
+
+app = createApp('')
+
+@app.route('/', methods = ["GET"])
 def home():
     databaseUri = os.getenv('DATABASE', '/repo/db/sqlite.db')
     conn = SqliteDB(databaseUri)
@@ -24,3 +37,15 @@ def home():
             </body>
         </html>
     ''' % ret[0][0]
+
+@app.route('/', methods = ["POST"])
+def addExercise():
+  databaseUri = os.getenv('DATABASE', '/repo/db/sqlite.db')
+  conn = SqliteDB(databaseUri)
+  cursor = conn.cursor
+  name = "Exercício 1"
+  query1 = "INSERT INTO Exercises VALUES('{n}')".format(n = name)
+  cursor.execute(query1)
+  conn.commit()
+  conn.close()
+  
