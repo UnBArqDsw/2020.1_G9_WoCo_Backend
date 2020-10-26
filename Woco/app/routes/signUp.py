9e3bbaf90782a7abdcb869d60daa4775e8bf97f8
Mexazonic <brunnohenriq697@gmail.com>
2020-10-26 14:55:01 -0300

    Fix Login Id

--- Woco/app/routes/signUp.py
+++ Woco/app/routes/signUp.py
@@ -2,17 +2,17 @@ from flask import Blueprint, jsonify, request
 from ..db.database import db
 from ..models.signUp import SignUp
 from ..serialisers.signUpSerialiser import SignUpSerializer
-import shortuuid
+import uuid
+
+
 def createUserBlueprint(blueprintName: str):
     blueprint = Blueprint(blueprintName, __name__)
 
     @blueprint.route('/api/user', methods = ["POST"])
     def addUser():
         data = request.get_json()
-        #key = uuid.uuid4()
-        key = "9997"
         
-        newUser = SignUp(id = str(key), username = data["username"], email = data["email"], password = data["password"])
+        newUser = SignUp(id = str(uuid.uuid4()), username = data["username"], email = data["email"], password = data["password"])
         
         db.session.add(newUser)
         db.session.commit()
